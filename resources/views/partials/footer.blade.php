e22dfa54f697dcb9cb05dfea201cd847b31b69ad
Roberto <r.attanasio90@gmail.com>
2021-05-06 17:22:32 +0200

    Footer completato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,73 @@
 <footer>
-    sono il footer
+    <div class="wrapper">
+        <div class="footer-col">
+            <img class="logo"src="{{ asset('img/logo.png') }}" alt="">
+        </div>
+        <div class="footer-col">
+            <ul>
+                <li class="title-blue">Pastificio</li>
+
+                <li>Il Pastificio</li>
+                <li>Grano decorticato a pietra</li>
+                <li>Il Molise c'è</li>
+                <li>Filiera Integrata</li>
+                <li>100 anni di pasta</li>
+                <li>Sartoria della pasta</li>
+                <li>Spaghetto Quadrato</li>
+                <li>Le Persone</li>
+            </ul>
+            <ul>
+                <li class="title-blue">Prodotti</li>
+
+                <li>Il Pastificio</li>
+                <li>Grano decorticato a pietra</li>
+                <li>Il Molise c'è</li>
+                <li>Filiera Integrata</li>
+                <li>100 anni di pasta</li>
+                <li>Sartoria della pasta</li>
+                <li>Spaghetto Quadrato</li>
+                <li>Le Persone</li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <ul>
+                <li class="title-blue">Collezione da Chef</li>
+
+                <li>Collezione da Chef</li>
+                <li>Grandi Cucine</li>
+                <li>Biologiche</li>
+                <li>Quadrate</li>
+            </ul>
+
+            <ul>
+                <li class="title-grey">Limited Edition</li>
+            </ul>
+
+            <ul>
+                <li class="title-grey">News</li>
+
+                <li class="news">e-commerce</li>
+                <li class="news">B2B</li>
+                <li class="news">Comunicazione</li>
+                <li class="news">Scarica i cataloghi</li>
+                <li class="news">Rassegna Stampa</li>
+                <li class="news">Video</li>
+                <li class="news">Contatti</li>
+                <li class="news">Lavora con noi</li>
+                <li class="news">Certificazioni</li>
+                <li class="news">Privacy Policy</li>
+            </ul>
+
+        </div>
+        <div class="footer-col">
+            <ul>
+                <li class="info"><strong>Ragione Sociale:</strong> LA MOLISANA S.P.A</li>
+                <li class="info"><strong>Sede Legale:</strong> CONTRADA</li>
+            </ul>
+            <img class="numbers" src="img/numero-verde.jpeg" alt="">
+            <img class="numbers" src="img/whatsapp.jpeg" alt="">
+
+        </div>
+    </div>
+    <div class="bottom-footer"></div>
 </footer>
